a3ceb6c3f003703a33eaee5c448eed422fb13492
C Martin <contact@cmartin.io>
2024-05-11 14:36:18 +0200

    worked on the isolation of the navbar and slow start to the implementation of javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+
+//window.onload = function() {
+    // Code for loading the navbar
+  //  document.getElementById("navbar").innerHTML =
+    //   '<object type="text/html" data="navbar.html" ></object>';
+//};
